15f630386d7dd933bc84c8a1c36b19dbc5a2d5c6
Gibs <gilbertappiah@icloud.com>
2024-12-23 00:09:06 +0000

    Add provision script

--- provision.sh
+++ provision.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env bash
 
-RESOURCEGROUP=$(az group list --query "[0].name" --output tsv)
+RESOURCEGROUP=onpremtoAppSvcDemo
 USERNAME=mslearnuser
-PASSWORD="Aa1#$(openssl rand -hex 5)"
+PASSWORD=Aa1#12ae850de4
 DEPLOYMENTNAME=ExerciseEnvironment
-
-# Create parameter file
-PARAM_FILE="parameters.json"
-cat << EOF > $PARAM_FILE
-{
-  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#",
-  "contentVersion": "1.0.0.0",
-  "parameters": {
-    "username": {
-      "value": "$USERNAME"
-    },
-    "password": {
-      "value": "$PASSWORD"
-    }
-  }
-}
-EOF
 
 echo "Starting provisioning..."
 
@@ -28,7 +11,7 @@ az deployment group create \
   --name $DEPLOYMENTNAME \
   --resource-group "$RESOURCEGROUP" \
   --template-uri "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-app-service-migration-assistant/master/azuredeploy.json" \
-  --parameters @$PARAM_FILE \
+  --parameters username=$USERNAME password=$PASSWORD \
   --no-wait
 
 echo "#!/usr/bin/env bash
@@ -41,20 +24,22 @@ POLL=\"az deployment group show \\
 
 RESULT=\$(eval \$POLL)
 STATUS=\"Waiting for provisioning to complete...\"
-until [ \"\$RESULT\" != \"Running\" ] && [ \"\$RESULT\" != \"Accepted\" ]; do
+until [ \"\$RESULT\" != \"Running\" ]
+do
   echo -n \"\$STATUS\"
   sleep 10
   STATUS=\".\"
   RESULT=\$(eval \$POLL)
 done
 
-if [ \"\$RESULT\" == \"Succeeded\" ]; then
+if [ \"\$RESULT\" == \"Succeeded\" ]
+then
   echo \"Provisioning complete!\"
-  echo -e \"\\nVM credentials:\"
+  echo -e \"\nVM credentials:\"
   echo \"Username: $USERNAME\"
   echo \"Password: $PASSWORD\"
 else
-  echo \"Provisioning failed with status: \$RESULT\"
+  echo \"Provisioning failed\"
 fi
 " > finish.sh
 
